4e80396041a2d8f99bb70c4f01b423c3b673d332
SheperoMah <26594373+SheperoMah@users.noreply.github.com>
2018-07-02 10:42:42 +0300

    MPI IV 9ab

--- heat/mpi-ip2p/c/core.c
+++ heat/mpi-ip2p/c/core.c
@@ -11,11 +11,72 @@
 /* Exchange the boundary values */
 /* only start the communication in background */
 void exchange_init(field *temperature, parallel_data *parallel)
-{
+{   int sendUp, sendDown, receiveUp, receiveDown, sendUpTag, sendDownTag;
+    MPI_Request sendUpRequest, sendDownRequest, recvUpRequest, recvDownRequest;
+
+
+    if (parallel->rank == 0){ 
+       /* Upper most rank */
+       sendUp = receiveUp = MPI_PROC_NULL;
+       sendDown = receiveDown = parallel->nup;
+       sendUpTag = 999999;
+       sendDownTag = parallel->rank+1;
+
+    } else if( parallel->rank == parallel->size-1){
+        /* Lower most rank */
+       sendUp = receiveUp = parallel->nup;       
+       sendDown = receiveDown = MPI_PROC_NULL;
+       sendUpTag = parallel->rank-1;
+       sendDownTag = 9999999;
+
+ 
+    } else {
+       /* Intermidate ranks */
+       sendUp = receiveUp = parallel->nup;
+       sendDown = receiveDown = parallel->ndown;
+       sendUpTag = parallel->rank-1;
+       sendDownTag = parallel->rank+1;
+    }
+
+
+
     // Send to the up, receive from down
     // TODO
+    MPI_Isend(&temperature->data[1][0], // the first non-ghost row that I have
+              temperature->ny, //count
+              MPI_DOUBLE, //datatype
+              sendUp, //dest
+              sendUpTag, //tag
+              MPI_COMM_WORLD, //comm
+              &parallel->requests[0]); //request
+
+    MPI_Irecv(&temperature->data[temperature->nx+1][0],
+              temperature->ny, //count
+              MPI_DOUBLE, //datatype
+              receiveDown, //source
+              parallel->rank, //tag
+              MPI_COMM_WORLD, //comm
+              &parallel->requests[1]); //request
+
     // Send to the down, receive from up
     // TODO
+    MPI_Isend(&temperature->data[temperature->nx][0],
+               temperature->ny, //count
+               MPI_DOUBLE, //type
+               sendDown, //dest
+               sendDownTag, //tag
+               MPI_COMM_WORLD, //comm
+               &parallel->requests[2]); //request
+
+   MPI_Irecv(&temperature->data[0][0], 
+             temperature->ny, //count
+             MPI_DOUBLE, //type
+             receiveUp, //source
+             parallel->rank, //tag
+             MPI_COMM_WORLD, //comm
+             &parallel->requests[3]); //request
+
+
 }
 
 
@@ -30,17 +91,31 @@ void evolve_interior(field *curr, field *prev, double a, double dt)
      * As we have fixed boundary conditions, the outermost gridpoints
      * are not updated. */
     /* TODO */
+    dx2 = prev->dx * prev->dx;
+    dy2 = prev->dy * prev->dy;
+    for (i = 1; i < curr->nx + 1; i++) {
+        for (j = 1; j < curr->ny + 1; j++) {
+            curr->data[i][j] = prev->data[i][j] + a * dt *
+                               ((prev->data[i + 1][j] -
+                                 2.0 * prev->data[i][j] +
+                                 prev->data[i - 1][j]) / dx2 +
+                                (prev->data[i][j + 1] -
+                                 2.0 * prev->data[i][j] +
+                                 prev->data[i][j - 1]) / dy2);
+        }
+    }
+
 }
 
 /* complete the non-blocking communication */
-/* TODO */(
+/* TODO */
     void exchange_finalize(parallel_data *parallel)
 {
-
+    MPI_Waitall(4, parallel->requests, MPI_STATUS_IGNORE);
 }
 
 /* Update the temperature values using five-point stencil */
-/* update only the border-dependent regions of the field */
+/* updat:e only the border-dependent regions of the field */
 /* TODO */
 void evolve_edges(field *curr, field *prev, double a, double dt)
 {
--- mpi/datatypes/c/custom_type_a-b.c
+++ mpi/datatypes/c/custom_type_a-b.c
@@ -6,6 +6,8 @@ int main(int argc, char **argv)
     int rank;
     int array[8][8];
     //TODO: Declare a variable storing the MPI datatype
+    MPI_Datatype column;
+    MPI_Datatype index;
 
     int i, j;
 
@@ -38,15 +40,50 @@ int main(int argc, char **argv)
     }
 
     //TODO: Create datatype that describes one column. Use MPI_Type_vector.
+    MPI_Type_vector(8, 1, 8, MPI_INT, &column);
+    MPI_Type_commit(&column);
+    
+    int displs[4];// = {0,17,33,51};
+    int blocklens[4] = {1, 2, 3, 4};
+
+    for (i=0; i < 4; i++){ 
+        displs[i] = i + 2*i*8;
+        }
+
+    MPI_Type_indexed(4, blocklens, displs, MPI_INT, &index);
+    MPI_Type_commit(&index);
 
     //TODO: Send first column of matrix form rank 0 to rank 1
+    MPI_Request request;
+
+    if (rank == 0){
+       MPI_Send(&array[0][1], 1, column, 1, 9090, MPI_COMM_WORLD);  
+       MPI_Send(&array[0][0], 1, index, 2, 9080, MPI_COMM_WORLD);
+    }
+    if (rank == 1){
+        MPI_Recv(&array[0][1], 1, column, 0, 9090, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    }
+    if (rank ==2){
+       MPI_Recv(&array[0][0], 1, index, 0, 9080, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
 
     //TODO: free datatype
-
+    MPI_Type_free(&column);
+    MPI_Type_free(&index);
     // Print out the result on rank 1
     // The application is correct if the first column has the values of rank 0
-    if (rank == 1) {
-        printf("Received data\n");
+    /*if (rank == 1) {
+        printf("Received data on rank 1\n");
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 8; j++) {
+                printf("%3d", array[i][j]);
+            }
+            printf("\n");
+        }
+    }*/
+
+   if (rank == 2) {
+        printf("Received data on rank 2\n");
         for (i = 0; i < 8; i++) {
             for (j = 0; j < 8; j++) {
                 printf("%3d", array[i][j]);
